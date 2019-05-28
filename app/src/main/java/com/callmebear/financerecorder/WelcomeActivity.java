feb0d080cbf2f3d97b5136a72541007a531f77b0
xuke <228619770@qq.com>
2019-05-28 17:58:46 +0800

    [commit]判断自然流量和非自然流量

--- app/src/main/java/com/callmebear/financerecorder/WelcomeActivity.java
+++ app/src/main/java/com/callmebear/financerecorder/WelcomeActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
+import com.callmebear.financerecorder.vest.MainVestActivity;
+import com.callmebear.financerecorder.vest.utils.PreferencesUtil;
 
 public class WelcomeActivity extends Activity {
-    private final long SPLASH_LENGTH = 1500;
+    private final long SPLASH_LENGTH = 3000;
     Handler handler = new Handler();
 
     public void onCreate(Bundle savedInstanceState) {
@@ -15,11 +19,18 @@ public class WelcomeActivity extends Activity {
         handler.postDelayed(new Runnable() {  //使用handler的postDelayed实现延时跳转
 
             public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String status = PreferencesUtil.getInstance().getTabType();
+                if ("Organic".equalsIgnoreCase(status)) {
+                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(WelcomeActivity.this, MainVestActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }, SPLASH_LENGTH);//2秒后跳转至应用主界面MainActivity
+        }, SPLASH_LENGTH);//3秒后跳转至应用主界面MainActivity
 
     }
 
